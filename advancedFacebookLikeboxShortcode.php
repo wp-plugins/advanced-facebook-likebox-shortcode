6c84b51627699c3b4ba1c0131986e94ae62e1ec3
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-20 06:03:56 +0000

    adding intial version
    git-svn-id: https://plugins.svn.wordpress.org/advanced-facebook-likebox-shortcode/trunk@878560 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ advancedFacebookLikeboxShortcode.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package advanced-facebook-likebox-shortcode
+*/
+/*
+Plugin Name: Advanced Facebook  Shortcode
+Plugin URI: http://www.sparxseo.com
+Description: Advanced Custom Facebook Likebox Shortcode is a customizable facebook likebox wordpress widget. There have lots of options to customize the likebox shortcode. So by using this you can up and run a customizable wordpress widget on your website very easily.
+Version: 1.0
+Author: Alan Ferdinand
+Author URI: http://www.sparxseo.com
+*/
+// Register style sheet.
+add_shortcode('advFacebookBox', 'advancedFacebookLikeboxShortcode');
+ function advancedFacebookLikeboxShortcode($atts){
+ 	$atts = shortcode_atts(array(
+ 		'suffix' => '',
+ 		'page' => 'http://www.facebook.com/FacebookDevelopers',
+ 		'width' => '300',
+        'height' => '356',
+        'background' => '#050026',
+        'header' => 'false',
+		'padding' => '15',
+		'radius' => '15',
+        'border' => '2',
+		'bordercolor' => '#9A1717',
+        'theme' => 'light',
+		'author' => 'true'
+ 	), $atts);
+ 	extract($atts);
+
+ 	$data = "";
+        $data .= "
+            <script>(function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = '//connect.facebook.net/en_US/all.js#xfbml=1';
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
+             </script>
+  ";
+ 	$data .= "
+    <div id='advanced_facebook_likebox_shortcode' class='$suffix' style='max-width: $width";
+        $data .= "px;'>
+		<div class='fbCustom' style='background:$background; width:$width";
+        $data .= "px; padding: $padding";
+        $data .= "px; border: $border";
+        $data .= "px solid $bordercolor; border-radius: $radius";
+        $data .= "px;'>
+        <div class='FBWrap' style='height:$height";
+        $data .= "px; overflow: hidden;'>
+            <div class='fb-like-box'
+                data-width='$width' data-height='$height' 
+                data-href='$page'
+                data-border-color='$bordercolor' data-show-faces='true'
+                data-stream='false' data-header='false' data-color-scheme='dark'>
+            </div>
+         </div>
+      </div>
+</div>                   
+";
+if($author == "true"){
+	$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.sparxseo.com' target='_blank' style='color: #808080;' title='Powered by SparxSEO'>SparxSEO.COM</a></div>";}
+    return $data;
+ }
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Advanced Facebook Likebox Shortcode ===
+
+Plugin URI: http://www.sparxseo.com
+Author Name : Alan Ferdinand
+Author URL : http://www.sparxseo.com
+Tags: advanced facebook likebox shortcode, wordpress shortcode plugin, advanced facebook, custom facebook, facebook likebox, facebook shortcode, facebook for wordpress, facebook widget for wordpress, super customizable facebook likebox
+Requires at least: 3.0.1
+Tested up to: 3.8
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Advanced Facebook Likebox Shortcode is free facebook likebox display shortcode plugin. Using this plugin you can show facebook connections on your wordpress website.
+
+== Description ==
+
+Everybody enjoys corresponding with friends, sharing photos, and posting comments. Technology has truly played a momentous role in today’s age, and it has spread to all nations. Technology has evolved significantly over time, once very simplistic, and now, highly innovative and complex. In previous years, technology was a precious commodity intended for the elite and wealthy. Now, it can be accessed by individuals of all socioeconomic classes.
+
+Features of Advanced Facebook Likebox Shortcode
+
+	* Can set a custom background on your facebook likebox and many more custom options.
+	* Many customizable options you can ever think can possible with facebook likebox before.
+	* Highly Customizable - Just checkout documentation to know more about available atributes.
+	* Work with most of recent wordpress released versions.
+	* Responsive - it make sure you able to enjoy our widget on mobile devices as well.
+	* Two color schemes are available.
+
+== Installation ==
+
+Check the documentation folder. or go here - http://sparxseo.com/documentation/advanced-facebook-likebox-shortcode.html
+
+== Frequently Asked Questions ==
+ Contact us for any kind of support - admin@sparxseo.com
+
+== Screenshots ==
+
+1. Frontend Screenshot
+
+== Changelog ==
+
+= 1.0 =
+Stable version release - 19th March 2014
