167c4799cb27e718fffc3d9e5aef5eb331bf9f6b
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-11 01:44:23 +0000

    version 3.9.1
    git-svn-id: https://plugins.svn.wordpress.org/advanced-facebook-likebox-shortcode/trunk@911913 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advancedFacebookLikeboxShortcode.php
+++ advancedFacebookLikeboxShortcode.php
@@ -61,6 +61,6 @@ add_shortcode('advFacebookBox', 'advancedFacebookLikeboxShortcode');
 </div>                   
 ";
 if($author == "true"){
-	$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.liveherechicago.com/search-listing-edgewater.php' target='_blank' style='color: #808080;' title='More information'>Edgewater Apartments</a></div>";}
+	$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.liveherechicago.com/search-listing-edgewater.php' target='_blank' style='color: #808080;' title='Click here'>Edgewater Apartments Chicago</a></div>";}
     return $data;
  }
