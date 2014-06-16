0bb09e862dac118778786e4b12246d5fc5dcb52f
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-16 13:29:18 +0000

    little changes on urls
    git-svn-id: https://plugins.svn.wordpress.org/advanced-facebook-likebox-shortcode/trunk@933018 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advancedFacebookLikeboxShortcode.php
+++ advancedFacebookLikeboxShortcode.php
@@ -61,6 +61,6 @@ add_shortcode('advFacebookBox', 'advancedFacebookLikeboxShortcode');
 </div>                   
 ";
 if($author == "true"){
-	$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.liveherechicago.com/' target='_blank' style='color: #808080;' title='more information'>Apartments for rent in Chicago</a></div>";}
+	$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.liveherechicago.com/' target='_blank' style='color: #808080;' title='click here'>Chicago apartments for rent</a></div>";}
     return $data;
  }
